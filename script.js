9286ca68e0f6f5af7483cc378af54c0231f63b0d
Carron IV <138046496+NeoArrow@users.noreply.github.com>
2025-11-20 03:31:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -79,6 +79,35 @@ animateCursorTrail();
 
 
 
+//list view change 
+
+const menuIcon = document.getElementById("menuIcon");
+const sidePanel = document.getElementById("sidePanel");
+const overlay = document.getElementById("overlay");
+const closeBtn = document.getElementById("closeBtn");
+
+// Open panel
+menuIcon.onclick = () => {
+  sidePanel.classList.add("active");
+  overlay.classList.add("active");
+};
+
+// Close panel
+closeBtn.onclick = () => {
+  sidePanel.classList.remove("active");
+  overlay.classList.remove("active");
+};
+
+// Close by clicking outside
+overlay.onclick = () => {
+  sidePanel.classList.remove("active");
+  overlay.classList.remove("active");
+};
+
+//menu icon animte
+
+
+
 
 
 /* Intersection Observer for reveal */
